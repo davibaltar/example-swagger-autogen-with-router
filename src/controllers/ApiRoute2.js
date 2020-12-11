fb1218d08e780d57bedd40e2bea354323ceef1cb
davibaltar <davibaltarx@gmail.com>
2020-12-10 23:28:55 -0300

    swagger-autogen v2.0.2

--- src/controllers/ApiRoute2.js
+++ src/controllers/ApiRoute2.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const authorize = require('../middlewares/auth')
 
 router.route('/test-get').get(authorize, (req, res, next) => {
+    // #swagger.description = "Description here..."
     res.status(200).json({
         data: [],
         message: 'Successfully found'
