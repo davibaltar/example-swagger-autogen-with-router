08a561510e223f717b16fdfa3b59c9aa1094bbe5
davibaltar <davibaltarx@gmail.com>
2021-12-26 23:44:28 -0300

    swagger-autogen 2.18.1 and refactoring

--- swagger.js
+++ swagger.js
@@ -57,7 +57,7 @@ const doc = {
     }
 }
 
-const outputFile = './swagger_output.json'
+const outputFile = './swagger-output.json'
 const endpointsFiles = ['./src/index.js']
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
